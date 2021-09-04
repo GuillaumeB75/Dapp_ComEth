d4d79482a04fc8c8344a386a791b7a345a4abf18
GuillaumeB75 <guillaume.bezie@yahoo.com>
2021-09-04 12:23:24 +0200

    add readme + prettier formating on some files

--- src/components/atoms/BudgetButton.js
+++ src/components/atoms/BudgetButton.js
@@ -1,15 +1,17 @@
-import React from 'react';
-import {  Button} from "@chakra-ui/react";
-import {  Link } from "react-router-dom";
+import React from "react";
+import { Button } from "@chakra-ui/react";
+import { Link } from "react-router-dom";
 
 const BudgetButton = () => {
-    return (
-        <>
-            <Link to="/budget">
-                <Button  boxShadow="dark-lg" margin="0.5rem">Budget</Button>
-            </Link>
-        </>
-    );
+  return (
+    <>
+      <Link to="/budget">
+        <Button boxShadow="dark-lg" margin="0.5rem">
+          Budget
+        </Button>
+      </Link>
+    </>
+  );
 };
 
 export default BudgetButton;
--- src/components/templates/HomeTemplate.js
+++ src/components/templates/HomeTemplate.js
@@ -1,13 +1,11 @@
 import { Center, Heading } from "@chakra-ui/react";
 import HomeBody from "../organisms/HomeBody";
 
-
 const HomeTemplate = () => {
   return (
     <>
-      
       <Center>
-      <Heading>Welcome Home</Heading>
+        <Heading>Welcome Home</Heading>
       </Center>
       <HomeBody />
     </>
